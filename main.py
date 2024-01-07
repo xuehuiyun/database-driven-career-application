c95d73b039af2592b305fec514d38afc3dfc8858
xuehuiyun <114709062+xuehuiyun@users.noreply.github.com>
2024-01-07 05:49:09 +0000

    added html and related css for home page

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "Hello World123!"
-
+    return render_template('home.html')
 print(__name__)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
