988c33443ab8a5b383dcdf6146e4333069784bc5
xuehuiyun <114709062+xuehuiyun@users.noreply.github.com>
2024-01-17 21:07:07 +0000

    connected it to mySQL db

---
+++ database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine, text
+import os
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+engine = create_engine(
+  db_connection_string,
+  connect_args={
+      "ssl": {
+        "ssl_ca": "/etc/ssl/cert.pem"
+      }
+  }
+)
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      row_dict = row._asdict()
+      jobs.append(row_dict)
+    return jobs
+
+    # print("type(result):", type(result))
+    # result_all = result.all()
+    # print("type(result.all()):", type(result_all))
+    # first_result = result_all[0]
+    # print("type(first_result):", type(first_result))
+    # first_result_dict = first_result._asdict()
+    # print("type(first_result_dict):", type(first_result_dict))
+    # print(first_result_dict)
+      
+    
--- main.py
+++ main.py
@@ -1,45 +1,52 @@
 from flask import Flask, render_template,jsonify
+from database import load_jobs_from_db
 app = Flask(__name__)
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Frontend Engineer',
-    'location': ' Seattle',
-    'salary':'$100,000'
-  },
-  {
-    'id': 2,
-    'title': 'Backend Engineer',
-    'location': ' Los Angeles',
-    'salary':'$100,000'
-  },
-  {
-    'id': 3,
-    'title': 'Fullstack Engineer',
-    'location': ' San Jose',
-    'salary':'$120,000'
-  },
-  {
-    'id': 4,
-    'title': 'Frontend Engineer Intern',
-    'location': ' Remote'
-  },
-  {
-    'id': 5,
-    'title': 'Data Analyst',
-    'location': ' New York',
-    'salary':'$80,000'
-  },
-]
+# JOBS = [
+#   {
+#     'id': 1,
+#     'title': 'Frontend Engineer',
+#     'location': ' Seattle',
+#     'salary':'$100,000'
+#   },
+#   {
+#     'id': 2,
+#     'title': 'Backend Engineer',
+#     'location': ' Los Angeles',
+#     'salary':'$100,000'
+#   },
+#   {
+#     'id': 3,
+#     'title': 'Fullstack Engineer',
+#     'location': ' San Jose',
+#     'salary':'$120,000'
+#   },
+#   {
+#     'id': 4,
+#     'title': 'Frontend Engineer Intern',
+#     'location': ' Remote'
+#   },
+#   {
+#     'id': 5,
+#     'title': 'Data Analyst',
+#     'location': ' New York',
+#     'salary':'$80,000'
+#   },
+# ]
+
+
+  
 @app.route('/')
 def hello_world():
-    return render_template('home.html',jobs=JOBS)
+    jobs = load_jobs_from_db()
+    return render_template('home.html',jobs=jobs)
 @app.route('/api/jobs')
 def list_jobs():
-  return jsonify(JOBS)
+  return jsonify(jobs)
 @app.route('/event.html')
 def event():
     return render_template('event.html')
-  
+@app.route('/register.html')
+def register():
+    return render_template('register.html')  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
